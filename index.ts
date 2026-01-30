d76509eb94153b601381d45f30af826c7de45373
XPlasma <ahmadalfakeh973@gmail.com>
2026-01-30 18:45:29 +0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,14 @@
+export {
+  AntigravityOAuthPlugin
+} from "./src/plugin";
+
+export {
+  authorizeAntigravity,
+  exchangeAntigravity
+} from "./src/antigravity/oauth";
+
+export type {
+  AntigravityAuthorization,
+  AntigravityTokenExchangeResult
+} from "./src/antigravity/oauth";
+
